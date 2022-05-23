b0a0bf5315621930480bb3ca5da0faca767e6c35
stpn-lmbn <stephenlumban@gmail.com>
2022-05-23 13:40:19 +0800

    Create iFirebaseAuth.cs

---
+++ ChatApp_LumbanKuizon/ChatApp_LumbanKuizon/DependencyServices/iFirebaseAuth.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+
+namespace ChatApp_LumbanKuizon.DependencyServices
+{
+    public interface iFirebaseAuth
+    {
+        Task<Models.FirebaseAuthResponseModel> LoginWithEmailPassword(string email, string password);
+        Task<Models.FirebaseAuthResponseModel> SignUpWithEmailPassword(string name, string email, string password);
+        Task<Models.FirebaseAuthResponseModel> SignOut();
+        Task<Models.FirebaseAuthResponseModel> IsLoggedIn();
+        Task<Models.FirebaseAuthResponseModel> ResetPassword(string email);
+    }
+}
