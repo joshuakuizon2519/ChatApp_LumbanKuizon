a5877a1983bc594cfa4e6730b983708387d6c12b
stpn-lmbn <stephenlumban@gmail.com>
2022-05-25 13:40:24 +0800

    Update ProfilePage.xaml.cs

--- ChatApp_LumbanKuizon/ChatApp_LumbanKuizon/Pages/ProfilePage.xaml.cs
+++ ChatApp_LumbanKuizon/ChatApp_LumbanKuizon/Pages/ProfilePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ChatApp_LumbanKuizon.DependencyServices;
+using ChatApp_LumbanKuizon.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +22,21 @@ namespace ChatApp_LumbanKuizon.Pages
         private async void BacktoRoot(object sender, EventArgs e)
         {
             await Navigation.PopToRootAsync();
+        }
+
+        private async void Logout_Button_Clicked(object sender, EventArgs e)
+        {
+            FirebaseAuthResponseModel res = new FirebaseAuthResponseModel() { };
+            res = await DependencyService.Get<iFirebaseAuth>().SignOut();
+
+            if(res.Status == true)
+            {
+                App.Current.MainPage = new NavigationPage(new TabbedMain());
+            }
+            else
+            {
+                await DisplayAlert("Error", res.Response, "Okay");
+            }
         }
     }
 }
