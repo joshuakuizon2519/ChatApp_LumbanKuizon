d623a82ce8059ae5f669e948a3ad856361b1e17d
stpn-lmbn <stephenlumban@gmail.com>
2022-05-25 13:40:35 +0800

    Update ForgotPassw.xaml.cs

--- ChatApp_LumbanKuizon/ChatApp_LumbanKuizon/Pages/ForgotPassw.xaml.cs
+++ ChatApp_LumbanKuizon/ChatApp_LumbanKuizon/Pages/ForgotPassw.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ChatApp_LumbanKuizon.DependencyServices;
+using ChatApp_LumbanKuizon.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,31 @@ namespace ChatApp_LumbanKuizon.Pages
         public ForgotPassw()
         {
             InitializeComponent();
+        }
+
+        private async void SendEmail_Clicked(object sender, EventArgs e)
+        {
+            if(Email.Text.Length == 0)
+            {
+                await DisplayAlert("Error", "Missing Field/s", "Okay");
+            }
+            else
+            {
+                //loadingVisible = true;
+                FirebaseAuthResponseModel res = new FirebaseAuthResponseModel() { };
+                res = await DependencyService.Get<iFirebaseAuth>().ResetPassword(Email.Text);
+
+                if(res.Status == true)
+                {
+                    await DisplayAlert("Succes", res.Response, "Okay");
+                    await Navigation.PopModalAsync();
+                }
+                else
+                {
+                    await DisplayAlert("Error", res.Response, "Okay");
+                }
+                //loadingVisible = false;
+            }
         }
     }
 }
