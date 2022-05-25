afab089be5a8f05fbe5961946c57bc2ae9775217
stpn-lmbn <stephenlumban@gmail.com>
2022-05-25 20:55:57 +0800

    Update app.xaml

---
+++ ChatApp_LumbanKuizon/ChatApp_LumbanKuizon/Converter/IsOwnerConverter.cs
@@ -0,0 +1,30 @@
+﻿using ChatApp_LumbanKuizon.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ChatApp_LumbanKuizon.Converter
+{
+    public class IsOwnerConverter
+    {
+        DataClass dataClass = DataClass.GetInstance;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool retVal = false;
+            string[] players = value as string[];
+
+            if (players[0].Equals(dataClass.loggedInUser.uid))
+            {
+                retVal = true;
+            }
+            return retVal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
