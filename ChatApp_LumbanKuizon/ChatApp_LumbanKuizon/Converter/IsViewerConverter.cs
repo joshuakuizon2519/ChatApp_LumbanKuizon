21d4920e263cac8f3101474ba975c37932ba013f
stpn-lmbn <stephenlumban@gmail.com>
2022-05-30 12:10:58 +0800

    Create IsViewerConverter.cs

---
+++ ChatApp_LumbanKuizon/ChatApp_LumbanKuizon/Converter/IsViewerConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatApp_LumbanKuizon.Converter
+{
+    public class IsViewerConverter : IValueConverter
+    {
+        DataClass dataClass = DataClass.GetInstance;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool retVal = false;
+
+            if(value != null)
+            {
+                ConversationModel conversation = value as ConversationModel;
+                if(conversation.converseeID.Equals(dataClass.loggedInUser.uid))
+                    retVal = true;
+            }
+            return retVal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
