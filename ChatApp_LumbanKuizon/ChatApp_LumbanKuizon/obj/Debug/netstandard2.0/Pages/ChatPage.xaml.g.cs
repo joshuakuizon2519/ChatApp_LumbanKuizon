6a4547ce19a96306b995b6bf48f02680d0641a1c
stpn-lmbn <stephenlumban@gmail.com>
2022-05-30 12:10:52 +0800

    Update ChatPage.xaml.g.cs

--- ChatApp_LumbanKuizon/ChatApp_LumbanKuizon/obj/Debug/netstandard2.0/Pages/ChatPage.xaml.g.cs
+++ ChatApp_LumbanKuizon/ChatApp_LumbanKuizon/obj/Debug/netstandard2.0/Pages/ChatPage.xaml.g.cs
@@ -17,8 +17,16 @@ namespace ChatApp_LumbanKuizon.Pages {
     public partial class ChatPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.ListView contactsList;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.ListView conversationListView;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(ChatPage));
+            contactsList = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "contactsList");
+            conversationListView = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "conversationListView");
         }
     }
 }
